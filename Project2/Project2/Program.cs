ae9141cef99240a00cbabc4ab50ef9e90777a0b3
Easton <eastoncox9@gmail.com>
2018-04-12 08:46:39 -0600

    name

---
+++ Project2/Project2/Program.cs
@@ -0,0 +1,29 @@
+﻿// File/Project Prolog
+// Name:     Easton Cox && other kid
+// Period:   A1
+// Username: coxeas000
+// Project:  Project2
+// Date:     4/12/2018 8:42:13 AM
+//
+// I declare that the following code was written by me or provided 
+// by the instructor for this project. I understand that copying source
+// code from any other source constitutes cheating, and that I will receive
+// a zero on this project if I am found in violation of this policy.
+// ---------------------------------------------------------------------------
+
+using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Your code goes below here
+
+            // Your code goes above here
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
